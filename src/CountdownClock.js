323c6de6ecefb0b658a7ff4e0cea5efa263b07d3
Jon Washington <jonathan.washington@gmail.com>
2019-03-24 14:02:29 -0700

    working on countdown function

---
+++ src/CountdownClock.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const runCountdown = (minutes_to_count) => {
+  var seconds = 60;
+  // for the sake of testing this timer we're setting a default time of 25 minutes. Once we can get the tomato to count down from 25, then we'll bind it to a button press, which will then pass the 25 to the function.
+  var mins = 25;
+  function tick(){
+    var current_mins = mins - 1;
+    seconds--;
+    if (seconds > 0) {
+      setTimeout (tick, 1000);
+    } else {
+      if(mins > 1){
+        runCountdown(mins-1);
+      }
+    }
+  }
+}
+
+const CountdownClock = (props) => {
+  return(
+    <div></div>
+  )
+}
