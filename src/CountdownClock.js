cbdb630df0e0de4f07b64baa84546d678e730c3a
Jon Washington <jonathan.washington@gmail.com>
2019-03-31 16:52:02 -0700

    minute and second differentiation being tinkered with

--- src/CountdownClock.js
+++ src/CountdownClock.js
@@ -3,9 +3,10 @@ import React from 'react';
 const runCountdown = (minutes_to_count) => {
   var seconds = 60;
   // for the sake of testing this timer we're setting a default time of 25 minutes. Once we can get the tomato to count down from 25, then we'll bind it to a button press, which will then pass the 25 to the function.
-  var mins = 1;
+  var mins = minutes_to_count;
   function tick(){
     var current_mins = mins - 1;
+    this.setState({ time: current_mins })
     seconds--;
     if (seconds > 0) {
       setTimeout (tick, 1000);
@@ -21,16 +22,12 @@ const runCountdown = (minutes_to_count) => {
 }
 
 class CountdownClock extends React.Component {
-  state = {timerTime: 5};
+  state = {timerTime: 1};
 
   componentDidMount(){
-    setInterval(() => {
-      // if (this.state.timerTime < 0){
-      //   console.log('hiya!');
-      // }
+      runCountdown(this.state.timerTime);
       var thisTime = (this.state.timerTime--);
       this.setState({ time: thisTime})
-    }, 1000)
   }
   render(){
     return(
