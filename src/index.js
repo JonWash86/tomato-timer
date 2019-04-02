37be97e1aa1511fe7f79fe61f72da5dfc90ae1e6
Jon Washington <jonathan.washington@gmail.com>
2019-04-02 11:33:38 -0700

    successfully called runTimer

--- src/index.js
+++ src/index.js
@@ -2,25 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CountdownClock from './CountdownClock';
 
+
 class App extends React.Component {
   state = {mins: '2', secs: '00'};
 
   // this function automatically displays the current time on an interval, mainly added to test displaying a timecode on the tomato.
   componentDidMount() {
+    this.runTimer();
+  }
 
-    this.setState({ mins: (this.state.mins - 1), secs:'09' });
+  runTimer = () => {
     var tickTock = setInterval(() => {
-      if (this.state.mins >= 0 && this.state.secs > 0){
-        this.setState({ secs: (this.state.secs - 1)});
+      if (this.state.mins > 0 && this.state.secs === 0 ){
+        this.setState({mins: this.state.mins - 1, secs: 59})
+      }
+      else if (this.state.mins >= 0 && this.state.secs > 0){
+        var newSec = (this.state.secs - 1);
+        if (newSec < 10){
+          newSec = ("0" + newSec);
+        }
+        this.setState({ secs: newSec});
       }
       if (this.state.mins === 0 && this.state.secs ===0){
-
         clearInterval(tickTock);
       }
+      else {
+
+      }
     }, 1000)
+    this.setState({ mins: (this.state.mins - 1), secs:'19' });
   }
 
   render(){
+
     return(
   <CountdownClock mins={this.state.mins} secs={this.state.secs}/>);
   }
