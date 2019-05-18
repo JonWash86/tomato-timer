16d294bbf547146e9eeccd096aa73bd66a3a2fe1
Jon Washington <jonathan.washington@gmail.com>
2019-05-18 12:16:09 -0700

    handleclick function is now discerning whether an active timer is running

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import TimerButton from './TimerButton';
 
 
 class App extends React.Component {
-  state = {mins: '25', secs: '00'};
+  state = {mins: '25', secs: '00', on: null};
 
   // this function automatically displays the current time on an interval, mainly added to test displaying a timecode on the tomato.
   componentDidMount() {
@@ -18,18 +18,53 @@ class App extends React.Component {
     console.log('a rooty ');
   };
 
-  runTimer = (duration) => {
-    this.setState({mins: duration, secs: '00'});
-    // $(':button').on('click', clearInterval(tickTock));
+  handleClick = (duration) => {
+    console.log(duration);
+    if(!this.state.on){
+      console.log('the click has not been clicked');
+      this.runTimer(duration);
+    }
+    else{
+      // clearInterval(tickTock)
+    };
   }
+
+  stopTimer = () => {
+
+  }
+
+  runTimer = (duration) => {
+    this.setState({on: 'yes'});
+
+    this.setState({mins: duration, secs: '00'});
+
+    // clearInterval(tickTock);
+    const tickTock = setInterval(() => {
+      if (this.state.mins > 0 && this.state.secs == 0 ){
+        this.setState({mins: this.state.mins - 1, secs: 59});
+      }
+      else if (this.state.mins >= 0 && this.state.secs > 0){
+        var newSec = (this.state.secs - 1);
+        if (newSec < 10){
+          newSec = ("0" + newSec);
+        }
+        this.setState({ secs: newSec});
+      }
+      else if (this.state.mins === 0 && this.state.secs ===0){
+        clearInterval(tickTock);
+      }
+      }, 1000);
+  }
+
+
 
 
   render(){
     return(<div>
       <CountdownClock  mins={this.state.mins} secs={this.state.secs} />
-      <TimerButton duration ={'25'} name="Start" onClick={this.runTimer}/>
-      <TimerButton name="Pomodoro" duration ={'25'} onClick={this.runTimer}/>
-      <TimerButton name="Short Break"duration ={'5'} onClick={this.runTimer} />
+      <TimerButton duration ={'25'} name="Start" onClick={this.handleClick}/>
+      <TimerButton name="Pomodoro" duration ={'25'} onClick={this.handleClick}/>
+      <TimerButton name="Short Break" duration ={'5'} onClick={this.handleClick} />
       </div>
     );
   }
