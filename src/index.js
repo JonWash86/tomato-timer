65a2af3aa9559efa1d8cd988bf3f7194b214c541
Jon Washington <jonathan.washington@gmail.com>
2019-05-18 12:26:56 -0700

    by binding the interval to state I was able to halt the active timer and start the new one

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import TimerButton from './TimerButton';
 
 
 class App extends React.Component {
-  state = {mins: '25', secs: '00', on: null};
+  state = {mins: '25', secs: '00', on: null, tickTock: null};
 
   // this function automatically displays the current time on an interval, mainly added to test displaying a timecode on the tomato.
   componentDidMount() {
@@ -18,14 +18,15 @@ class App extends React.Component {
     console.log('a rooty ');
   };
 
+
   handleClick = (duration) => {
     console.log(duration);
-    if(!this.state.on){
-      console.log('the click has not been clicked');
+    if(!this.state.tickTock){
       this.runTimer(duration);
     }
     else{
-      // clearInterval(tickTock)
+      clearInterval(this.state.tickTock);
+      this.runTimer(duration);
     };
   }
 
@@ -39,21 +40,24 @@ class App extends React.Component {
     this.setState({mins: duration, secs: '00'});
 
     // clearInterval(tickTock);
-    const tickTock = setInterval(() => {
-      if (this.state.mins > 0 && this.state.secs == 0 ){
-        this.setState({mins: this.state.mins - 1, secs: 59});
-      }
-      else if (this.state.mins >= 0 && this.state.secs > 0){
-        var newSec = (this.state.secs - 1);
-        if (newSec < 10){
-          newSec = ("0" + newSec);
+    this.setState({
+      tickTock: setInterval(() => {
+        if (this.state.mins > 0 && this.state.secs == 0 ){
+          this.setState({mins: this.state.mins - 1, secs: 59});
         }
-        this.setState({ secs: newSec});
-      }
-      else if (this.state.mins === 0 && this.state.secs ===0){
-        clearInterval(tickTock);
-      }
-      }, 1000);
+        else if (this.state.mins >= 0 && this.state.secs > 0){
+          var newSec = (this.state.secs - 1);
+          if (newSec < 10){
+            newSec = ("0" + newSec);
+          }
+          this.setState({ secs: newSec});
+        }
+        else if (this.state.mins === 0 && this.state.secs ===0){
+          clearInterval(this.state.tickTock);
+        }
+      }, 1000)
+
+    })
   }
 
 
