e71837b2bfcf417ad3cf8e2b75c6ee44f954fea2
Jon Washington <jonathan.washington@gmail.com>
2019-06-02 13:49:41 -0700

    enabling javascript

--- src/StatsZone.js
+++ src/StatsZone.js
@@ -1,19 +1,5 @@
 import React from 'react';
 
-// class StatsZone extends React.Component {
-//   constructor(props){
-//     super(props);
-//   }
-//
-//   render(){
-//     return(<div className="statsZone">Time-mater
-//     Pomodoros:
-//     Short Breaks:
-//     Long Breaks:
-//     </div>);
-//   }
-// }
-
 const StatsZone = (props) => {
   return(
     <div className="statsZone"><h1 id="title">Time-'Mater</h1> <hr/>
